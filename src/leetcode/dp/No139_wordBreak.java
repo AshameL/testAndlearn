ae4b561ab6ff26b63cd3aea5e9bb027a8418b7ad
dell_deepin <lzyzsere@126.com>
2019-02-25 10:31:04 +0800

    0225

---
+++ src/leetcode/dp/No139_wordBreak.java
@@ -0,0 +1,51 @@
+package leetcode.dp;
+
+import java.util.*;
+
+public class No139_wordBreak {
+    public boolean wordBreak(String s, List<String> dict) {
+        if (dict.contains(s))
+            return true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        // 使用set去检查去除重复计算
+        // 这是时间复杂度降到O(N^2)的关键
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0);
+        while (!queue.isEmpty()) {
+            int curIdx = queue.poll();
+            for (int i = curIdx + 1; i <= s.length(); i++) {
+                if (visited.contains(i))
+                    continue;
+                if (dict.contains(s.substring(curIdx, i))) {
+                    // 如果达到中重点
+                    // 注意此时的i是curIdx+1
+                    if (i == s.length())
+                        return true;
+                    queue.offer(i);
+                    visited.add(i);
+                }
+            }
+        }
+        return false;
+
+    }
+
+    //dp 解法
+
+    public boolean wordBreak_dp(String s, List<String> dict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+
+            }
+        }
+        return dp[s.length() + 1];
+    }
+}
---
+++ src/leetcode/dp/No96_searchTree.java
@@ -0,0 +1,20 @@
+package leetcode.dp;
+
+public class No96_searchTree {
+    //https://blog.csdn.net/qq_38595487/article/details/79598628
+    public int numTrees(int n) {
+        if (n <= 1)
+            return n;
+
+        int[] nums = new int[n + 1];
+        nums[0] = 1;
+        nums[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            for (int j = 0; j < i; j++) {
+                nums[i] = nums[i] + nums[j] * nums[i - 1 - j];
+            }
+
+        return nums[n];
+    }
+}
