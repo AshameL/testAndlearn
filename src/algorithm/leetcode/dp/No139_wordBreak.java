0608cc984d6e0552ad5fb56e4efb60c0895ac810
Li DongYuan(焦点) <dongyuanli217597@sohu-inc.com>
2019-07-10 09:27:45 +0800

    word break系列

--- src/algorithm/leetcode/dp/No139_wordBreak.java
+++ src/algorithm/leetcode/dp/No139_wordBreak.java
@@ -2,6 +2,25 @@ package algorithm.leetcode.dp;
 
 import java.util.*;
 
+/**
+ * 第一次：
+ * 第二次：2019-07-09
+ *      自上而下
+ *      wordBreak("leetcode") = inDict("leetcode")
+ *                          ||wordBreak("l") && inDict("eetcode")
+ *                          ||wordBreak("le") && inDict("etcode")
+ *                          ||wordBreak("lee") && inDict("tcode")
+ *                          ||wordBreak("leet") && inDict("code")
+ *                          ||......
+ *
+ *       自下而上
+ *       dp[i]: 0- i-1 有多少个分割，构成s[0,i-1]这样的数组
+ *       dp[i] = dp[0] && inDicts(s[0...i-1])
+ *              || dp[1]&&inDict(s[i,...i-1])
+ *              ...  dp[j]&&inDict(s[j,i-1]) j<=i-1
+ *
+ *       trick 对比单词大于当前剩余字单词时，直接跳过即可。
+ */
 public class No139_wordBreak {
     public boolean wordBreak(String s, List<String> dict) {
         if (dict.contains(s))
@@ -31,8 +50,7 @@ public class No139_wordBreak {
 
     }
 
-    //digui 解法
-
+    //dp 解法
     public boolean wordBreak_dp(String s, List<String> dict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
@@ -43,9 +61,8 @@ public class No139_wordBreak {
                     dp[i] = true;
                     break;
                 }
-
             }
         }
-        return dp[s.length() + 1];
+        return dp[s.length() ];
     }
 }
---
+++ src/algorithm/leetcode/dp/No140_wordBreak2.java
@@ -0,0 +1,30 @@
+package algorithm.leetcode.dp;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class No140_wordBreak2 {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return dfs(s,wordDict,new HashMap<String,LinkedList<String>>());
+    }
+    List<String> dfs (String s, List<String>wordDict, HashMap<String, LinkedList<String>>map){
+        if(map.containsKey(s)){
+            return map.get(s);
+        }
+        LinkedList<String> res= new LinkedList<>();
+        if(s.length()==0){
+            res.add("");
+            return res;
+        }
+        for(String word:wordDict){
+            if(s.startsWith(word)){
+                List<String>sublist = dfs(s.substring(word.length()),wordDict,map);
+                for(String sub:sublist)
+                    res.add(word + (sub.isEmpty() ? "" : " ")+sub);
+            }
+        }
+        map.put(s,res);
+        return res;
+    }
+}
